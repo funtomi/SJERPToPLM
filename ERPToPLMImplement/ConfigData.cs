302f081ba618ed3a5c8b7c17a3344475d276bf46
funtomi <396166153@qq.com>
2018-10-25 14:22:49 +0800

    Initial commit

---
+++ ERPToPLMImplement/ConfigData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ERPToPLMImplement {
+    class ConfigData {
+        public ConfigData(XmlDocument doc) {
+            if (doc==null) {
+                throw new ArgumentNullException("doc");
+            }
+            InitData(doc);
+        }
+        private static string PATH_CONFIG = @"config";
+        private static string PATH_URL = @"URL";
+        private static string PATH_NAME = @"name";
+        private static string PATH_PWD = @"password";
+        private static string PATH_CLTID = @"clientId";
+        #region 配置字段
+        
+        /// <summary>
+        /// url
+        /// </summary>
+        public string Url {
+            get { return _url; }
+        }
+        private string _url;
+
+        /// <summary>
+        /// 登录账号
+        /// </summary>
+        public string LoginName {
+            get { return _loginName; }
+        }
+        private string _loginName;
+        
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string LoginPassword {
+            get { return _loginPassword; }
+        }
+        private string _loginPassword;
+
+        /// <summary>
+        /// clientId
+        /// </summary>
+        public int ClientId {
+            get { return _clientId; }
+        }
+        private int _clientId;
+        #endregion
+
+        /// <summary>
+        /// 读取xml配置的数据
+        /// </summary>
+        /// <param name="doc"></param>
+        private void InitData(XmlDocument doc) {
+            if (doc==null) {
+                return;
+            }
+            var configNode = doc.SelectSingleNode(PATH_CONFIG);
+            if (configNode==null||configNode.ChildNodes==null||configNode.ChildNodes.Count==0) {
+                return;
+            }
+            var urlNode = configNode.SelectSingleNode(PATH_URL);
+            _url = urlNode == null || string.IsNullOrEmpty(urlNode.InnerText) ? "" : urlNode.InnerText;
+            var nameNode = configNode.SelectSingleNode(PATH_NAME);
+            _loginName = nameNode == null || string.IsNullOrEmpty(nameNode.InnerText) ? "" : nameNode.InnerText;
+            var pwdNode = configNode.SelectSingleNode(PATH_PWD);
+            _loginPassword = pwdNode == null || string.IsNullOrEmpty(pwdNode.InnerText) ? "" : pwdNode.InnerText;
+            var cltIdName = configNode.SelectSingleNode(PATH_CLTID);
+            _clientId = cltIdName == null || string.IsNullOrEmpty(cltIdName.InnerText) ? 0 : Convert.ToInt32(cltIdName.InnerText);
+        
+        }
+    }
+}
---
+++ ERPToPLMImplement/ERPServiceHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace ERPToPLMImplement {
+    public class ERPServiceHelper {
+        private ERPServiceHelper() {
+            _configDoc = GetConfigDoc();
+            _configData = new ConfigData(_configDoc);
+        }
+        static ERPServiceHelper() {
+            Instance = new ERPServiceHelper();
+        }
+
+        public static ERPServiceHelper Instance;
+        private static string CONFIG_PATH = "config.xml"; 
+
+        public XmlDocument ConfigDoc {
+            get {
+                if (_configDoc==null) {
+                    _configDoc = GetConfigDoc();
+                    _configData = new ConfigData(_configDoc);
+                }
+                return _configDoc; }
+        }
+        private XmlDocument _configDoc;
+
+        private ConfigData _configData;
+        /// <summary>
+        /// 获取配置文档
+        /// </summary>
+        /// <returns></returns>
+        private XmlDocument GetConfigDoc() {
+            if (!File.Exists(CONFIG_PATH)) {
+                throw new ArgumentNullException(CONFIG_PATH);
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(CONFIG_PATH);
+            return doc;
+        }
+         
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public string Login(string username, string password, int clientId) {
+            //return _service.login("ad", "123456", 1003);
+
+            //string url = "http://192.168.91.131:8080/acc/services/ERPService";
+            try {
+                object[] args = new object[3];
+                args[0] = username;
+                args[1] = password;
+                args[2] = clientId;
+                object result = WebServiceHelper.InvokeWebService(_configData.Url, "login", args);
+                return result.ToString(); 
+            } catch (Exception) {
+                throw;
+            }
+            
+        }
+
+        /// <summary>
+        /// 使用配置信息登录
+        /// </summary>
+        /// <returns></returns>
+        public string LoginWithConfig() {
+            return Login(_configData.LoginName,_configData.LoginPassword,_configData.ClientId);
+        }
+
+        public void Logout(int uid) { 
+        
+        }
+
+        public void SaveBase(string username, string password, int clientid, int uid,int baseid, string keyvalue, string[] fieldvalues) { }
+
+        public void OpenReport(string username, string password, int clientid, int uid,int repid, string requeststr) { }
+
+
+    }
+}
